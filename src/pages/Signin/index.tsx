cfea17c127b9908f02b1a8a6a71f38f4bc0a3505
unknown <theopinheiro12@gmail.com>
2023-11-28 17:49:43 -0400

    pagina de login agora é funcional

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,18 +1,56 @@
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo copy.svg';
+import api from '../../services/api';
 import './styles.css';
-//const Logo:string = require('../../assets/logo copy.svg');
+
 
 function SignIn(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        try {
+            if(!email){
+                throw new Error('Email is required');
+            }
+            if(!password){
+                throw new Error('Password is required');
+            }
+            const response = await api.post('/login',{
+                email,
+                password
+            })
+
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
 
     return(
         <div className='container container-sign-in'>
             <div className='sign-in'>
             <img src={Logo} alt='logo' />
 
-                <form>
-                    <input type="text" placeholder='E-mail'/>
-                    <input type="password" placeholder='Password'/>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    type="text" 
+                    placeholder='E-mail'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                    <input 
+                    type="password" 
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
 
                     <span>
                         Não tem cadastro?  
